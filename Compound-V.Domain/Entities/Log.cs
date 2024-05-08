d080a999691112265ef0fc40a9b71587792e4ce0
Krzysiek Jaworski <krzysztof.jaworski98@gmail.com>
2024-05-08 07:25:31 +0200

    working with entities

---
+++ Compound-V.Domain/Entities/Log.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compound_V.Domain.Entities
+{
+    public class Log
+    {
+        public Guid Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string? StackTrace { get; set; }
+        public string? Exception{ get; set; }
+    }
+}
