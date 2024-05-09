1f3044e3ddc46782327a4abfbcf07c6d5392a5a6
Krzysiek Jaworski <krzysztof.jaworski98@gmail.com>
2024-05-09 07:32:20 +0200

    add EF

---
+++ Compound-V.Infrastructure/Persistence/CompoundDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compound_V.Infrastructure.Persistence
+{
+    internal class Compound_V_DbContext
+    {
+    }
+}
