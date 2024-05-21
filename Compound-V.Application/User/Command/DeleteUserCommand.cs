da1ce5718f48ecc83bbad324e89314221839ec59
Krzysiek Jaworski <krzysztof.jaworski98@gmail.com>
2024-05-21 07:18:26 +0200

    impelemnt, unassign user-role, delete user, create user

---
+++ Compound-V.Application/User/Command/DeleteUserCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compound_V.Application.User.Command
+{
+    public class DeleteUserCommand : IRequest
+    {
+        public string UserEmail { get; set; } = default!;
+    }
+}
