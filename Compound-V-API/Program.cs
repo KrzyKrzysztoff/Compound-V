2efbdd992521396ceab405da01ab67c2586823a6
Krzysiek Jaworski <krzysztof.jaworski98@gmail.com>
2024-06-05 07:00:56 +0200

    work with exception middleware

--- Compound-V-API/Program.cs
+++ Compound-V-API/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
 using Serilog;
+using Compound_V_API.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -68,7 +69,11 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
+builder.Services.AddExceptionHandler<ExceptionHandler>();
+
 var app = builder.Build();
+
+app.UseExceptionHandler();
 
 var scope = app.Services.CreateScope();
 
